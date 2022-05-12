d12be6d063104c5c8cff7f8346cc2be3720b7dcb
ajuan54 <97407823+ajuan54@users.noreply.github.com>
2022-05-11 20:50:20 -0700

    1st save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  function updateFName(event) {
+    const firstName = event.target.value;
+    setFName(firstName);
+  }
+  function updateLName(event) {
+    const lastName = event.target.value;
+    setLName(lastName);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fName} {lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={updateFName}
+          name="fName"
+          placeholder="First Name"
+          value={fName}
+        />
+        <input
+          onChange={updateLName}
+          name="lName"
+          placeholder="Last Name"
+          value={lName}
+        />
         <button>Submit</button>
       </form>
     </div>
